c2c419e86c21f7b3b6f468af0728cde22456c178
COEDK-CODER <paritaladineshkumar@gmail.com>
2020-03-08 10:50:31 +0530

    use the resources for routong conevntions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,16 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
-    render plain: Todo.all.to_a
+    render plain: Todo.order(:due_date).map { |todo| todo.to_formatted_string }.join("\n")
+  end
+
+  def show
+    render plain: Todo.find(params[:id]).to_formatted_string
+  end
+
+  def create
+    new_todo = Todo.create!(todo_text: params[:todo_text], due_date: Date.parse(params[:due_date]), completed: "false")
+    render plain: "A new todo added is with ID_NO #{new_todo.id}"
   end
 end
--- app/models/concerns/todo.rb
+++ app/models/concerns/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+  def to_formatted_string
+    status = completed ? "[x]" : "[]"
+    "#{id} #{due_date.to_s(:long)} #{todo_text} #{status}"
+  end
 end
