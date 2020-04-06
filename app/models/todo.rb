38f2a389579b5daee4aeb8e71e199ddf44810c89
COEDK-CODER <paritaladineshkumar@gmail.com>
2020-04-06 06:59:57 +0530

    updated database migrations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,23 @@
+require "date"
+
 class Todo < ActiveRecord::Base
   def to_formatted_string
     status = completed ? "[x]" : "[]"
     "#{id} #{due_date.to_s(:long)} #{todo_text} #{status}"
+  end
+
+  def self.overdue
+    all.where(" due_date< ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date> ?", Date.today)
+  end
+  def self.completed
+    all.where("completed= ?", true)
   end
 end
